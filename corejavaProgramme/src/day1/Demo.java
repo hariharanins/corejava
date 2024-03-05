6452dcd99e4f98991c01aceafbdb9418bb6d6ca4
Jeganathan <Jeganathan@DESKTOP-S38J3N5>
2024-03-05 15:47:18 +0530

    this is the java code basics

---
+++ corejavaProgramme/src/day1/Demo.java
@@ -0,0 +1,18 @@
+package day1;
+import java.util.*;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Scanner n= new Scanner(System.in);
+		int a,b,c;
+		System.out.println("enter the value of a and b");
+		a=n.nextInt();
+		b=n.nextInt();
+		c=a+b;
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
